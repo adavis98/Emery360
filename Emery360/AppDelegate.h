27b21beace571fc4ad206c4d7b0e9c8c3a156ee4
Adam Davis <adamdavis@Adams-MacBook-Pro.local>
2015-05-02 00:58:05 -0500

    Initial Commit

---
+++ Emery360/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emery360
+//
+//  Created by Adam Davis on 5/2/15.
+//  Copyright (c) 2015 Adam Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Emery360/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emery360
+//
+//  Created by Adam Davis on 5/2/15.
+//  Copyright (c) 2015 Adam Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
